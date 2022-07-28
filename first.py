3c6f22a32362072302c489d8d64e6e684e39c9b0
gergorty <greg.galetto@gmail.com>
2022-07-28 10:50:18 -0400

    attempt w/ wsgi

--- first.py
+++ first.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
-    return render_template("index.html", content="Testing")
+    return render_template("index.html")
 
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=app.config['DEBUG']) 
